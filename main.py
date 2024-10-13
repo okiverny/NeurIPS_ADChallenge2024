325606442bd214af494c4c281ffb4809020862e0
okiverny <oleg.kivernyk@gmail.com>
2024-10-13 17:53:00 +0200

    Intro of Signal Extraction Strategy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 from signal_extraction import SignalExtractor
-from background_subtraction import ConstantBackgroundSubtraction, LinearBackgroundSubtraction, BackgroundSubtractionManager
+from background_subtraction import ConstantBackgroundSubtraction, LinearBackgroundSubtraction, QuadraticBackgroundSubtraction, BackgroundSubtractionManager
+from signal_extraction_strategy import ErfParametrization
 
 if __name__ == "__main__":
     # Loading data
@@ -11,11 +12,17 @@ if __name__ == "__main__":
     time_steps = np.arange(0, 187)  # Time steps
 
     # Instantiate the manager with the initial strategy (ConstantBackgroundSubtraction)
-    background_manager = BackgroundSubtractionManager(ConstantBackgroundSubtraction())
+    #background_manager = BackgroundSubtractionManager(ConstantBackgroundSubtraction())
 
     # Initialize the SignalExtractor with a constant background subtraction strategy
-    extractor = SignalExtractor(ConstantBackgroundSubtraction())
+    extractor = SignalExtractor(QuadraticBackgroundSubtraction(), ErfParametrization())
 
     # Extract signal using the constant strategy
-    data_normalized, quality_metric = extractor.extract_signal(data, planet_id)
+    data_normalized, quality_metric, a_vals, c_vals, t0_vals, sigma_vals, a_errs, *rest_errs = extractor.extract_signal(
+        data, planet_id
+    )
     print('Signal Spectrum (Const):', data_normalized)
+
+    # Compute predictions and statistical errors
+    preds = 2*np.abs(a_vals)[::-1]
+    preds_err = 3*np.abs(a_errs)[::-1]
