735e44c6eaf050ec0138a7312902c4310718ce47
okiverny <oleg.kivernyk@gmail.com>
2024-10-18 09:48:34 +0200

    Pol3 Bkg subtraction

--- background_subtraction.py
+++ background_subtraction.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 import numpy as np
 import scipy
 
-from helpers import linear_func, quadratic_func, calc_chisquare
+from helpers import linear_func, quadratic_func, pol3_func, calc_chisquare
 
 # Strategy interface
 class BackgroundSubtractionStrategy(ABC):
@@ -167,6 +167,61 @@ class QuadraticBackgroundSubtraction(BackgroundSubtractionStrategy):
         """Quality metric as chi2/ndof of the linear fit."""
         return np.all(quality_metric < 1000.0)
     
+class CubicBackgroundSubtraction(BackgroundSubtractionStrategy):
+    def subtract_background(self, data, planet_id, transit_breakpoint, lambda_step=1):
+        estimator = data[planet_id, :, :, :].sum(axis=2)   # Sum along spatial dimension, it contains signal+background.
+
+        # Setting the time points
+        total_time = data.shape[1]
+        transit_half_duration = total_time // 23 #  = 8 ; time window when the stellar flux is on the half-way darkening
+        midpoint = total_time // 2  # planet is at the middle
+        ingress_time_step = transit_breakpoint - transit_half_duration
+        egress_time_step = total_time - transit_breakpoint + transit_half_duration
+
+        # Define unobscured and obscured time steps
+        time_steps_unobscured_left = np.arange(0, ingress_time_step)
+        time_steps_unobscured_right = np.arange(egress_time_step, total_time)
+        time_steps_unobscured = np.concatenate((time_steps_unobscured_left, time_steps_unobscured_right))
+        time_steps = np.arange(0, total_time)
+
+        # Get the corresponding values
+        y_left = estimator[time_steps_unobscured_left, :]
+        y_right = estimator[time_steps_unobscured_right, :]
+        y = estimator[time_steps_unobscured, :]
+        y_err = np.ones_like(y) * np.minimum(y_left.std(axis=0), y_right.std(axis=0))
+
+        # Initialize the lists for important quantities
+        par0, par1 = [], []
+        par0_err, par1_err = [], []
+        quality_metric = []
+        data_normalized = []
+
+        # Loop over wavelengths
+        for lambda_i in range(y.shape[1]):
+            popt, pcov = scipy.optimize.curve_fit(pol3_func, time_steps_unobscured, y[:,lambda_i], p0=np.array([0, 50, -20000, 3.83e+08]), maxfev=800)
+            popt_err = np.sqrt(np.diag(pcov))
+
+            # Append fit results
+            par0.append(popt[0])
+            par1.append(popt[1])
+            par0_err.append(popt_err[0])
+            par1_err.append(popt_err[1])
+
+            # Compute and append the fit quality metric chi2/NDF
+            chi2 = calc_chisquare(y[:, lambda_i], y_err[:, lambda_i], pol3_func(time_steps_unobscured, *popt))
+            NDF = len(y) - len(popt)
+            quality_metric.append(chi2/NDF)
+
+            # Detrending the data (normalization by stellar flux)
+            data_normalized.append( estimator[:,lambda_i] / pol3_func(time_steps, *popt) )
+
+        # implement the method here
+        return np.array(data_normalized).T, np.array(quality_metric)
+
+    def check_quality(self, quality_metric):
+        """Quality metric as chi2/ndof of the linear fit."""
+        return np.all(quality_metric < 1000.0)
+
 # The manager not yet used in the implementation!
 class BackgroundSubtractionManager:
     """Manager Class to Control Strategy and Fallback"""
