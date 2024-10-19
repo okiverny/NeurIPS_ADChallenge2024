0c9b1d62acb4e156f535bfbb27293f148e49596c
okiverny <oleg.kivernyk@gmail.com>
2024-10-19 10:16:16 +0200

    Template Fit refinement

--- template_fit_strategy.py
+++ template_fit_strategy.py
@@ -1,8 +1,8 @@
 from signal_extraction_strategy import SignalExtractStrategy
 import numpy as np
-import warnings
 from iminuit import Minuit, cost
-from helpers import chi2_interpol_func, quadratic_func, find_uncertainty
+from helpers import chi2_interpol_func, quadratic_func, find_uncertainty, calc_chisquare
+from  plotting import plot_chi2_scan
 
 # Concrete Strategies
 class TemplateFitStrategy(SignalExtractStrategy):
@@ -55,7 +55,8 @@ class TemplateFitStrategy(SignalExtractStrategy):
 
         # Initial parameters for the fits
         initial_params = {'a': 50, 'b':-20000, 'c':3.83e+08}
-        init_pars_interpol = {'a': 1.0, 'b':0.002, 'c':0.01}
+        init_pars_interpol = {'a': 1.0, 'b':results_incl['mu'], 'c':0.01} if results_incl else {'a': 1.0, 'b':0.002, 'c':0.01}
+        pars_limits = [(0, None), (0, None), (0, None)]
 
 
         # Loop over wavelength and fit the Erf function to normalized data, using lambda_step
@@ -73,6 +74,7 @@ class TemplateFitStrategy(SignalExtractStrategy):
 
             # Create and test the templates
             chi2_values = []
+            bkg_pars = []
             for mu in mu_values:
                 y_template = np.concatenate((y_unobscured_left, (1+mu)*y_obscured, y_unobscured_right))
                 cost_function = cost.LeastSquares(x, y_template, y_template_errors, quadratic_func)
@@ -80,13 +82,32 @@ class TemplateFitStrategy(SignalExtractStrategy):
                 m_bkg.migrad()
                 m_bkg.hesse()
                 chi2_values.append(m_bkg.fval/(len(y_template)-len(initial_params)))
+                bkg_pars.append(m_bkg.values)
+
+            # Find minimum chi2 fit and its config for bkg function
+            print('Minimum chi2:', chi2_values[ np.argmin(np.array(chi2_values)) ])
+            #print('Best bkg pars:', bkg_pars[ np.argmin(np.array(chi2_values)) ])
+            best_bkg_pars = bkg_pars[ np.argmin(np.array(chi2_values)) ]
+
+            # Recompute chi2 with fixed bkg
+            chi2_fixed_values = []
+            ndf = len(y_unobscured_left) + len(y_unobscured_right) + len(y_obscured) - 3
+            for mu in mu_values:
+                y_template = np.concatenate((y_unobscured_left, (1+mu)*y_obscured, y_unobscured_right))
+                chi2 = calc_chisquare(y_template, y_template_errors, quadratic_func(x, *best_bkg_pars))
+                ndf = len(y_template) - len(best_bkg_pars)
+                #chi2_fixed_values.append(chi2/ndf)
+                chi2_fixed_values.append(chi2/ndf)
 
             # Fit the chi2 dependence vs mu
-            chi2_values = np.array(chi2_values)
+            chi2_values = np.array(chi2_fixed_values)
             chi2_errors = np.ones_like(chi2_values)*0.001
+            #chi2_fixed_values = np.array(chi2_fixed_values)
+
             try:
                 cost_func = cost.LeastSquares(mu_values, chi2_values, chi2_errors, chi2_interpol_func)
                 m = Minuit(cost_func,  **init_pars_interpol)
+                m.limits = pars_limits
                 m.migrad()
                 m.hesse()
                 #if not results_incl: m.visualize()
@@ -94,15 +115,32 @@ class TemplateFitStrategy(SignalExtractStrategy):
                 # Estimate uncertainty from the spread at chi2_min+1
                 best_mu, min_chi2 = m.values['b'], m.values['a']
                 fit_func = lambda x: chi2_interpol_func(x, *m.values)
-                uncertainty = find_uncertainty(fit_func, best_mu, min_chi2)
-                uncertainty = m.errors['b']
+                uncertainty = find_uncertainty(fit_func, best_mu, min_chi2, step=0.000001, ndf=ndf)
+                #uncertainty = m.errors['b']
+                #uncertainty = m.values['c']
 
                 results = {'mu': m.values['b'], 'mu_err': uncertainty}
                 print('Best mu=',m.values['b'], '+/-',uncertainty)
 
+                #####################
+                plot_chi2_scan(mu_values, chi2_values, m.values, ndf, planet_id, (ilambda, ilambda+lambda_step))
+                #####################
+                fig, ax = plt.subplots(figsize=(16, 5))
+                y_template = np.concatenate((y_unobscured_left, (1+m.values['b'])*y_obscured, y_unobscured_right))
+                ax.errorbar(x, y_template, y_template_errors, fmt="ok", label="data (best mu)")
+                ax.plot(x, quadratic_func(x, *best_bkg_pars), label="fit")
+                ax.set_xlabel('time units', fontsize=13)
+                ax.set_ylabel('Events', fontsize=13)
+                plt.tight_layout()
+                plt.show()
+                plt.close()
+                #####################
+
                 # Check for bad fits
-                if results['mu']>0.020:
-                    results = {'mu': results_incl['mu'], 'mu_err': results_incl['mu_err']}
+                fit_convergance_status = m.fmin.is_valid
+                if results['mu']>0.010 or not fit_convergance_status:
+                    print(f"Fit is bad for wavelength range {ilambda}-{ilambda+lambda_step}. Returning inclusive values for this range.")
+                    results = {'mu': results_incl['mu'], 'mu_err': results_incl['mu_err']} if results_incl else {'mu': 0.0005, 'mu_err': 0.0002}
 
             except (RuntimeError):
                 print(f"Fit failed for wavelength range {ilambda}-{ilambda+lambda_step}. Returning inclusive values for this range.")
@@ -130,6 +168,7 @@ class TemplateFitStrategy(SignalExtractStrategy):
             
             # Create and test the templates
             chi2_values = []
+            ndf = len(y_unobscured_left) + len(y_unobscured_right) + len(y_obscured) - 3
             for mu in mu_values:
                 y_template = np.concatenate((y_unobscured_left, (1+mu)*y_obscured, y_unobscured_right))
                 cost_function = cost.LeastSquares(x, y_template, y_template_errors, quadratic_func)
@@ -150,14 +189,16 @@ class TemplateFitStrategy(SignalExtractStrategy):
                 # Estimate uncertainty from the spread at chi2_min+1
                 best_mu, min_chi2 = m.values['b'], m.values['a']
                 fit_func = lambda x: chi2_interpol_func(x, *m.values)
-                uncertainty = find_uncertainty(fit_func, best_mu, min_chi2)
-                uncertainty = m.errors['b']
+                uncertainty = find_uncertainty(fit_func, best_mu, min_chi2, step=0.000001, ndf=ndf)
+                #uncertainty = m.errors['b']
 
                 results = {'mu': m.values['b'], 'mu_err': uncertainty}
 
                 # Check for bad fits
-                if results['mu']>0.015:
-                    results = {'mu': results_incl['mu'], 'mu_err': results_incl['mu_err']}
+                fit_convergance_status = m.fmin.is_valid
+                if results['mu']>0.010 or not fit_convergance_status:
+                    print(f"Fit is bad for the remaining wavelength bins.. Returning inclusive values for this range.")
+                    results = {'mu': results_incl['mu'], 'mu_err': results_incl['mu_err']} if results_incl else {'mu': 0.0005, 'mu_err': 0.0002}
 
             except (RuntimeError):
                 print(f"Fit failed for the remaining wavelength bins. Returning inclusive values for this range.")
